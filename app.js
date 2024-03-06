cacf56a254b895092e9e794ebdfe61624c5aff22
Chris Kim <133260386+chrisjk02@users.noreply.github.com>
2024-03-06 15:23:34 -0800

    Refactor function declarations to be outside of return object

--- app.js
+++ app.js
@@ -6,14 +6,9 @@ const Gameboard = (function () {
         ['', '', '']
     ];
 
-    return {
-        getCellValue(row, col) {
-            return board[row][col];
-        },
+    const getCellValue = (row, col) => { return board[row][col]; };
 
-        setCellValue(row, col, value) {
-            board[row][col] = value;
-        }
+    const setCellValue = (row, col, value) => { board[row][col] = value; };
 
-    };
+    return { getCellValue, setCellValue }
 })();
